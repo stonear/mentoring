62a71ac24a5814ca266ff53c376527161a663b08
sherry\ai <27681405+stonear@users.noreply.github.com>
2018-09-03 14:26:28 +0700

    Don't show mentor if year and smt not selected

--- application/views/admin/mentor.php
+++ application/views/admin/mentor.php
@@ -33,96 +33,70 @@
         </div>
     </div>
 </div>
-<div class="row clearfix">
-    <div class="col-xs-8">
-        <?php if($tahun_selected != NULL and $semester_selected != NULL): ?>
-            <?php if ($semester_selected == 1) $smt = 'Gasal' ?>
-            <?php if ($semester_selected == 2) $smt = 'Genap' ?>
-            <a href="<?php echo base_url(); ?>Admin/download_mentor/<?php echo $tahun_selected.'/'.$semester_selected ?>" class="btn bg-light-blue waves-effect" data-toggle="tooltip" data-placement="right" title="download data mentor"><i class="material-icons">file_download</i> Download Data Mentor Tahun Ajaran <?php echo $t->tahun ?>-<?php echo $t->tahun + 1 ?> Semester <?php echo $smt ?> (xlsx)</a>
-        <?php endif ?>
-    </div>
-    <div class="col-xs-4 text-right tambah-mentor">
-        <?php if($tahun_selected != NULL and $semester_selected != NULL): ?>
-            <button type="button" class="btn bg-light-blue waves-effect" data-toggle="tooltip" data-placement="left" title="tambah mentor"><i class="material-icons">add</i> Tambah Mentor</button>
-            <br><br>
-        <?php endif ?>
-    </div>
-    <div class="col-xs-12">
-        <div class="card">
-            <div class="header">
-                <h2>MENTOR</h2>
-            </div>
-            <div class="body">
-                <div class="table-responsive">
-                    <table class="table table-bordered table-striped table-hover dataTable js-exportable">
-                        <thead>
-                            <tr>
-                                <th>No.</th>
-                                <th>NRP</th>
-                                <th>Nama</th>
-                                <th class="text-right">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($mentor as $m): ?>
-                                <tr>
-                                    <td></td>
-                                    <td><?php echo $m->NRPmentor ?></td>
-                                    <td><?php echo $m->nama ?></td>
-                                    <td class="text-right">
-                                        <a href="<?php echo base_url(); ?>Admin/profil_mentor/<?php echo $m->NRPmentor.'/'.$tahun_selected.'/'.$semester_selected ?>" class="btn bg-blue btn-xs waves-effect" data-toggle="tooltip" data-placement="top" title="profil mentor"><i class="material-icons">search</i></a>
-                                        <button type="button" class="pass-mentor-<?php echo $m->NRPmentor ?> btn bg-green btn-xs waves-effect" data-toggle="tooltip" data-placement="top" title="ubah kata sandi mentor"><i class="material-icons">vpn_key</i></button>
-                                        <button type="button" class="hapus-mentor-<?php echo $m->NRPmentor ?> btn btn-danger btn-xs waves-effect" data-toggle="tooltip" data-placement="top" title="hapus mentor"><i class="material-icons">delete</i></button>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+<?php if($tahun_selected != NULL and $semester_selected != NULL): ?>
+    <div class="row clearfix">
+        <div class="col-xs-8">
+            <?php if($tahun_selected != NULL and $semester_selected != NULL): ?>
+                <?php if ($semester_selected == 1) $smt = 'Gasal' ?>
+                <?php if ($semester_selected == 2) $smt = 'Genap' ?>
+                <a href="<?php echo base_url(); ?>Admin/download_mentor/<?php echo $tahun_selected.'/'.$semester_selected ?>" class="btn bg-light-blue waves-effect" data-toggle="tooltip" data-placement="right" title="download data mentor"><i class="material-icons">file_download</i> Download Data Mentor Tahun Ajaran <?php echo $t->tahun ?>-<?php echo $t->tahun + 1 ?> Semester <?php echo $smt ?> (xlsx)</a>
+            <?php endif ?>
         </div>
-    </div>
-</div>
-<div class="modal fade" id="tambah-mentor" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <form autocomplete="off" role="form" action="<?php echo base_url(); ?>Admin/tambah_mentor/<?php echo $tahun_selected.'/'.$semester_selected ?>" method="post">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="defaultModalLabel">Tambah Mentor</h4>
+        <div class="col-xs-4 text-right tambah-mentor">
+            <?php if($tahun_selected != NULL and $semester_selected != NULL): ?>
+                <button type="button" class="btn bg-light-blue waves-effect" data-toggle="tooltip" data-placement="left" title="tambah mentor"><i class="material-icons">add</i> Tambah Mentor</button>
+                <br><br>
+            <?php endif ?>
+        </div>
+        <div class="col-xs-12">
+            <div class="card">
+                <div class="header">
+                    <h2>MENTOR</h2>
                 </div>
-                <div class="modal-body">
-                    <div class="row clearfix">
-                        <div class="col-xs-12">
-                            <div class="form-group">
-                                <div class="form-line">
-                                    <input type="text" name="nrp" class="form-control" placeholder="Masukkan NRP mentor" required>
-                                </div>
-                            </div>
-                        </div>
+                <div class="body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped table-hover dataTable js-exportable">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>NRP</th>
+                                    <th>Nama</th>
+                                    <th class="text-right">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($mentor as $m): ?>
+                                    <tr>
+                                        <td></td>
+                                        <td><?php echo $m->NRPmentor ?></td>
+                                        <td><?php echo $m->nama ?></td>
+                                        <td class="text-right">
+                                            <a href="<?php echo base_url(); ?>Admin/profil_mentor/<?php echo $m->NRPmentor.'/'.$tahun_selected.'/'.$semester_selected ?>" class="btn bg-blue btn-xs waves-effect" data-toggle="tooltip" data-placement="top" title="profil mentor"><i class="material-icons">search</i></a>
+                                            <button type="button" class="pass-mentor-<?php echo $m->NRPmentor ?> btn bg-green btn-xs waves-effect" data-toggle="tooltip" data-placement="top" title="ubah kata sandi mentor"><i class="material-icons">vpn_key</i></button>
+                                            <button type="button" class="hapus-mentor-<?php echo $m->NRPmentor ?> btn btn-danger btn-xs waves-effect" data-toggle="tooltip" data-placement="top" title="hapus mentor"><i class="material-icons">delete</i></button>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CANCEL</button>
-                    <button type="submit" class="btn bg-light-blue waves-effect">ADD</button>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
-</div>
-<?php foreach ($mentor as $m): ?>
-    <div class="modal fade" id="pass-mentor-<?php echo $m->NRPmentor ?>" tabindex="-1" role="dialog">
+    <div class="modal fade" id="tambah-mentor" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <form autocomplete="off" role="form" action="<?php echo base_url(); ?>Admin/update_mentor/<?php echo $m->NRPmentor.'/'.$tahun_selected.'/'.$semester_selected ?>" method="post">
+                <form autocomplete="off" role="form" action="<?php echo base_url(); ?>Admin/tambah_mentor/<?php echo $tahun_selected.'/'.$semester_selected ?>" method="post">
                     <div class="modal-header">
-                        <h4 class="modal-title" id="defaultModalLabel">Update Password Mentor</h4>
+                        <h4 class="modal-title" id="defaultModalLabel">Tambah Mentor</h4>
                     </div>
                     <div class="modal-body">
                         <div class="row clearfix">
                             <div class="col-xs-12">
                                 <div class="form-group">
                                     <div class="form-line">
-                                        <input type="password" name="password" class="form-control" required>
+                                        <input type="text" name="nrp" class="form-control" placeholder="Masukkan NRP mentor" required>
                                     </div>
                                 </div>
                             </div>
@@ -130,31 +104,59 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CANCEL</button>
-                        <button type="submit" class="btn bg-light-blue waves-effect">UPDATE</button>
+                        <button type="submit" class="btn bg-light-blue waves-effect">ADD</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-    <div class="modal fade" id="hapus-mentor-<?php echo $m->NRPmentor ?>" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content modal-col-red">
-                <form autocomplete="off" class="form-horizontal" role="form" action="<?php echo base_url(); ?>Admin/hapus_mentor/<?php echo $m->NRPmentor.'/'.$tahun_selected.'/'.$semester_selected ?>" method="post">
-                    <div class="modal-header">
-                        <h4 class="modal-title" id="defaultModalLabel">Hapus Mentor</h4>
-                    </div>
-                    <div class="modal-body">
-                        Apakah anda yakin ingin menghapus <?php echo $m->nama ?>?
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CANCEL</button>
-                        <button type="submit" class="btn btn-link waves-effect">HAPUS</button>
-                    </div>
-                </form>
+    <?php foreach ($mentor as $m): ?>
+        <div class="modal fade" id="pass-mentor-<?php echo $m->NRPmentor ?>" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form autocomplete="off" role="form" action="<?php echo base_url(); ?>Admin/update_mentor/<?php echo $m->NRPmentor.'/'.$tahun_selected.'/'.$semester_selected ?>" method="post">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="defaultModalLabel">Update Password Mentor</h4>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row clearfix">
+                                <div class="col-xs-12">
+                                    <div class="form-group">
+                                        <div class="form-line">
+                                            <input type="password" name="password" class="form-control" required>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CANCEL</button>
+                            <button type="submit" class="btn bg-light-blue waves-effect">UPDATE</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-<?php endforeach; ?>
+        <div class="modal fade" id="hapus-mentor-<?php echo $m->NRPmentor ?>" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content modal-col-red">
+                    <form autocomplete="off" class="form-horizontal" role="form" action="<?php echo base_url(); ?>Admin/hapus_mentor/<?php echo $m->NRPmentor.'/'.$tahun_selected.'/'.$semester_selected ?>" method="post">
+                        <div class="modal-header">
+                            <h4 class="modal-title" id="defaultModalLabel">Hapus Mentor</h4>
+                        </div>
+                        <div class="modal-body">
+                            Apakah anda yakin ingin menghapus <?php echo $m->nama ?>?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">CANCEL</button>
+                            <button type="submit" class="btn btn-link waves-effect">HAPUS</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+<?php endif ?>
 <script>
     $(function()
     {
